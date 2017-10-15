f78a7aaeb937c7afbd7dc0201c460f98996d2d6a
David Akre <dakre21@gmail.com>
2017-10-15 16:13:52 -0700

    Updating submit file

--- histogram/submit.sh
+++ histogram/submit.sh
@@ -1,4 +1,3 @@
-```bash 
 #!/bin/bash 
 #BSUB -n 32 
 #BSUB -o histogram.out 
@@ -6,16 +5,11 @@
 #BSUB -q "windfall" 
 #BSUB -J histogram
 #BSUB -R gpu 
-#--------------------------------------------------------------------- 
 
-if [[ -z ${input} || -z ${output} ]]; then
-    echo "Invalid input arguments"
-    echo "For example 'bsub -a input=<file_name> output=<file_name>'"
-    exit 0
-fi
+export IN_FILE="input_example.txt"
+export OUT_FILE="output_result.txt"
 
 module load cuda  
 cd /extra/dakre/cuda_histogram/histogram
-time ./histogram ${input} ${output} 
+time ./histogram ${IN_FILE} ${OUT_FILE}
 ###end of script 
-``` 
