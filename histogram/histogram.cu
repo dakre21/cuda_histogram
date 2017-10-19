acd7aeaad9a9b386a50b4d5a98fa509c7eeb064a
David Akre <dakre21@gmail.com>
2017-10-18 23:29:21 -0700

    Another temp commit

--- histogram/histogram.cu
+++ histogram/histogram.cu
@@ -19,7 +19,7 @@ a file given by the second argument.
 using namespace std;
 
 #define BLOCK_SIZE   1
-#define THREAD_SIZE  2
+#define THREAD_SIZE  4
 #define NUM_ELEMENTS 10 
 
 // Global declaration for FILEs
@@ -37,35 +37,35 @@ __global__ void calc_histogram(char* dbuff, unsigned int* dcount, unsigned int s
         stop_pos = size;
     }
 
-    __syncthreads();
+    //__syncthreads();
 
     for (unsigned int i = start_pos; i < stop_pos; i++) {
         // Increment counter per occurances
         if (dbuff[i] == '0') {
-            dcount[0] += 1;
+            lcount[0] += 1;
         } else if (dbuff[i] == '1') {
-            dcount[1] += 1;
+            lcount[1] += 1;
         } else if (dbuff[i] == '2') {
-            dcount[2] += 1;
+            lcount[2] += 1;
         } else if (dbuff[i] == '3') {
-            dcount[3] += 1;
+            lcount[3] += 1;
         } else if (dbuff[i] == '4') {
-            dcount[4] += 1;
+            lcount[4] += 1;
         } else if (dbuff[i] == '5') {
-            dcount[5] += 1;
+            lcount[5] += 1;
         } else if (dbuff[i] == '6') {
-            dcount[6] += 1;
+            lcount[6] += 1;
         } else if (dbuff[i] == '7') {
-            dcount[7] += 1;
+            lcount[7] += 1;
         } else if (dbuff[i] == '8') {
-            dcount[8] += 1;
+            lcount[8] += 1;
         } else if (dbuff[i] == '9') {
-            dcount[9] += 1;
+            lcount[9] += 1;
         }
-        __syncthreads();
+        //__syncthreads();
     }
 
-    /*dcount[0] += lcount[0];
+    dcount[0] += lcount[0];
     dcount[1] += lcount[1];
     dcount[2] += lcount[2];
     dcount[3] += lcount[3];
@@ -74,7 +74,7 @@ __global__ void calc_histogram(char* dbuff, unsigned int* dcount, unsigned int s
     dcount[6] += lcount[6];
     dcount[7] += lcount[7];
     dcount[8] += lcount[8];
-    dcount[9] += lcount[9];*/
+    dcount[9] += lcount[9];
 }
 
 void calc_histogram2(char* dbuff, unsigned int* dcount, unsigned int size, int block_id, float stride) {
